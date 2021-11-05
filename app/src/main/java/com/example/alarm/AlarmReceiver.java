88504013831169f06ccab5795d96863996d3faf2
Echa <sdxkh0203@gmail.com>
2021-11-05 19:54:44 +0800

    tugas 5

---
+++ app/src/main/java/com/example/alarm/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.example.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    MediaPlayer player;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm aktif!", Toast.LENGTH_LONG).show();
+        player = MediaPlayer.create(context, R.raw.alarm);
+        player.start();
+    }
+}
